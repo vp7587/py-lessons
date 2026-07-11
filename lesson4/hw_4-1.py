2a821d326b903219b1ce853e2537eb00c74ba67f
vp7587 <vp7587@gmail.com>
2026-07-11 10:31:31 +0300

    add hw_4-1-3

--- lesson4/hw_4-1.py
+++ lesson4/hw_4-1.py
@@ -1,47 +1,39 @@
-# i = 5
-# while i < 20:
-#     i += 1
-#     if i % 2 == 0:
-#         print(i)
-#         # continue
-#
-#     if i % 3 == 0:
-#         break
-
-
-
-
-print("The program will move all zero from the list to the end of the list.\n")
+#Програма ищет не только нули. Пользователь сам в начале вводит число, которое зотел бы переместить и массив,
+# в котором эточисло искать. Так Более универсально и интересно
+print("The program will move all numbers you'll enter next step from the list to the end of the list.\n")
 run = "y"
 while run == "y" or run == "Y"  or run == "YES" or run == "yes":
-    print("Would you like to continue?(Type y/yes if you do):")
-    run = input();
-    if run == "y" or run == "Y" or run == "YES" or run == "yes":
-        print("You should input a few numbers as string, including zero (0).\n")
-        raw_text= input("Please enter a few numbers like this: 0123456\n")
-        if raw_text:
-            if raw_text.isdigit():
-                numbers_list = list(raw_text)
-                count_numbers_list_elements = len(numbers_list)
-                print(f"Your list is not empty. Ok. Going ahead..")
-                if count_numbers_list_elements == 1:
-                    print(f"Your has only one item")
-                else:
-                    count_zero_in_list =  numbers_list.count("0")
-                    if count_zero_in_list == 0:
-                        print(f"Sorry, your list has no zero.")
+    run = str.lower(input("Would you like to continue?(Type y/yes if you do):\n"))
+    if run == "y" or run == "yes":
+        user_value_to_move = (input("Please, enter number you want to move: \n"))
+        if user_value_to_move.isdigit():
+            print("You should input a few numbers as string, including number you want to move.\n")
+            raw_text= input("Please enter a few numbers like this: 0123456\n")
+            if raw_text:
+                if raw_text.isdigit():
+                    numbers_list = list(raw_text)
+                    count_numbers_list_elements = len(numbers_list)
+                    print(f"Your list is not empty. Ok. Going ahead..")
+                    if count_numbers_list_elements == 1:
+                        print(f"Your has only one item")
                     else:
-                        print(f"Well, your list has {count_zero_in_list} zeros. Going ahead..")
-                        i = 0
-                        while i < count_zero_in_list:
-                            index_of_zero_in_list = numbers_list.index("0")
-                            numbers_list.remove("0")
-                            numbers_list.append("0")
-                            i += 1
-                        print(f"The final list is: {numbers_list}")
+                        user_value_to_move_in_list =  numbers_list.count(user_value_to_move)
+                        if user_value_to_move_in_list == 0:
+                            print(f"Sorry, your list has no your number.")
+                        else:
+                            print(f"Well, your list has {user_value_to_move_in_list} your numbers. Going ahead..")
+                            i = 0
+                            while i < user_value_to_move_in_list:
+                                index_of_user_value_to_move_in_list = numbers_list.index(user_value_to_move)
+                                numbers_list.remove(user_value_to_move)
+                                numbers_list.append(user_value_to_move)
+                                i += 1
+                            print(f"The final list is: {numbers_list}")
+                else:
+                    print("Sorry, invalid input. You should enter only numbers.")
             else:
-                print("Sorry, invalid input. You should enter only numbers.")
+                print("Your list is empty")
         else:
-            print("Your list is empty")
+            print("Sorry, invalid input. You should enter only numbers.")
     else:
         print("Exiting program... Good bye.")
