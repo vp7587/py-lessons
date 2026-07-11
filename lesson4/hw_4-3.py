ab6e55cee900344eed82adaf4334a3261d3b7e2b
vp7587 <vp7587@gmail.com>
2026-07-11 13:49:18 +0300

    add hw_4-3-1

---
+++ lesson4/hw_4-3.py
@@ -0,0 +1,14 @@
+import random
+print("The raw range with random elements quantity from 3 to 10 will be created \n"
+      "Using the raw range the final list will be formed \n"
+      "The final list should consists 1, 3 and the 2 from the range end\n")
+random_range_len = (random.randint(3, 10))
+print(f"Random raw range len: {random_range_len}")
+raw_range = list(range(random_range_len))
+print(f"Random raw range: {raw_range}")
+final_list = []
+final_list.append(raw_range[0])
+final_list.append(raw_range[2])
+final_list.append(raw_range[-2])
+print(f"Final list: {final_list}")
+
