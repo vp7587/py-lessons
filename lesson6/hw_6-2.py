f520ea07a610ae3158d9592f703454cc254d895c
vp7587 <vp7587@gmail.com>
2026-07-18 15:00:32 +0300

    hw_6-2.2

--- lesson6/hw_6-2.py
+++ lesson6/hw_6-2.py
@@ -5,14 +5,28 @@ while True:
     if run_program == "y" or run_program == "yes":
         user_string = input("Please enter integer number between 0 and 8640000.\n")
         #length check
-        if 0 < len(user_string) < 8640000:
+        if 1 > len(user_string):
             print("\nError: you enter less then 1 symbol. Try again\n")
             continue
         #is digit check
         if (user_string.isdigit() == False):
             print("\nError: you should put only digits.\n")
             continue
-
+        time = int(user_string)
+        #value check
+        if  0 < time < 8640000 == False:
+            print("\nError: your number is not between 0 and 8640000.\n")
+            continue
+        days = time // 86400
+        hours = str(time % 86400 // 3600)
+        minuts = str(time % 86400 % 3600 // 60)
+        seconds = str(time % 86400 % 3600 % 60)
+        #decide to print "days" or ""days
+        if  days % 2 != 0:
+            days_quantity = "day"
+        else:
+            days_quantity = "days"
+        print (f"Your time is: {days} {days_quantity}, {hours.zfill(2)}:{minuts.zfill(2)}:{seconds.zfill(2)}.")
     else:
         print("Good bye.")
         sys.exit()
