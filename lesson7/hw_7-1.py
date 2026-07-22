a3fb279eb9b26b7963a70d2a6a6626b6245e0358
vp7587 <vp7587@gmail.com>
2026-07-22 10:18:34 +0300

    done hw_7-1

--- lesson7/hw_7-1.py
+++ lesson7/hw_7-1.py
@@ -1,4 +1,16 @@
-def test(a,b):
-    return(a+b)
+import sys
+print("The program shows greeting message, according given name and age.")
+def say_hello(var_name, var_age) -> str:
+    result = f"Hello. My name is {var_name} and I'm {var_age} years old"
+    return result
 
-print (test(5,1))
+while True:
+    run_program = str.strip(str.lower(input("\nWould you like to continue?(Type y/yes if you do):\n")))
+    if run_program == "y" or run_program == "yes":
+        name = input("\nWhat is your name?\n").strip()
+        age = input("\nWhat is your age?\n").strip()
+        assert say_hello(name, age) == f"Hello. My name is {name} and I'm {age} years old", 'check name and age'
+        print(say_hello(name, age))
+    else:
+        print("Good bye.")
+        sys.exit()
