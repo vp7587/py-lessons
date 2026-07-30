2bfb44f29b9fd0af3a940b4018c14e115ae33f02
vp7587 <vp7587@gmail.com>
2026-07-30 09:36:26 +0300

    done hw_9-1

--- lesson9/hw_9-1.py
+++ lesson9/hw_9-1.py
@@ -1,24 +1,20 @@
 def popular_words (text, words):
-    text = (str(text)).strip().lower()
-    print(text)
     words = list(words)
+    words_in_text = (str(text)).strip().lower().split()
     result = {}
-    i = 1
-    count_words = text.count(" ")+1
-    while i <= count_words:
+    for raw_word in words_in_text:
         for word in words:
-            find = text.find(word)
-            if  find >0  and word in result:
-                result[word] = result[word] + 1
-            elif find >0 :
-                result[word] = 1
-                result.update({word: result[word]})
-            elif  word not in result:
-                result[word] = 0
-                result.update({word: result[word]})
-            text = text.replace(word, "", 1)
-        i = i + 1
+            if word in words_in_text:
+                find = words_in_text.index(word)
+                if find >= 0:
+                    if not word in result:
+                        result.update({word: 1})
+                    else:
+                        result[word] = result[word] + 1
+                words_in_text.remove(word)
+            elif word not in result:
+                result.update({word: 0})
     return result
 
-#assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1' print('OK')
-popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near'])
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
