13f894b002f8699727c5f964e137e0d424af50e6
vp7587 <vp7587@gmail.com>
2026-07-30 10:08:06 +0300

    done hw_9-2.0

--- lesson9/hw_9-2.py
+++ lesson9/hw_9-2.py
@@ -1,12 +1,15 @@
-import math
-
 def difference (*arg):
     arg_list = list(arg)
-    min_value = arg_list.index(min(arg_list))
-    return arg_list
-print (difference(1, 2, 3))
-# assert difference(1, 2, 3) == 2, 'Test1'
-# assert difference(5, -5) == 10, 'Test2'
-# assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
-# assert difference() == 0, 'Test4'
-# print('OK')
+    if arg_list:
+        min_value = min(arg_list)
+        max_value = max(arg_list)
+        result = round ((max_value - min_value), 2)
+    else:
+        result = 0
+    return result
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
