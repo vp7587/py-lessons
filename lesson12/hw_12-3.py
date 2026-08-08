69643d02119cb3435b95a404cd858cf5ef0b83e6
vp7587 <vp7587@gmail.com>
2026-08-08 15:26:33 +0300

    add hw_12-3.0

---
+++ lesson12/hw_12-3.py
@@ -0,0 +1,18 @@
+import re
+
+def is_even(number):
+    even_list = [2,4,6,8]
+    print(number)
+    last_number = re.search(r'.$', str(number))
+    last_number = last_number.group()
+
+    print(last_number)
+    if int(last_number) in even_list:
+        return True
+    else:
+        return False
+
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
