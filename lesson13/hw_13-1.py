709f6a74c211b77ee65f47e62017b7d736b18fea
vp7587 <vp7587@gmail.com>
2026-08-13 19:25:53 +0300

    add hw_13-1

---
+++ lesson13/hw_13-1.py
@@ -0,0 +1,42 @@
+# def create_file (file_name):
+#     file = open(file_name, 'w', encoding="utf-8")
+#     file.close()
+#
+# def read_the_file (file_name):
+#     file = open(file_name, 'r', encoding="utf-8")
+#     for line in file:
+#         print(line)
+#     file.close()
+
+# file_name = input("Enter file name: ")
+# create_file(file_name)
+
+import re
+
+def remove_tags (file_name):
+    file = open(file_name, 'r', encoding="utf-8")
+    for line in file:
+        newline = re.findall(r">(.*)</", line)
+        if newline != "[]":
+            print(newline)
+        # add_lines_to_file(file_name, newline)
+    file.close()
+
+def add_lines_to_file(file_name, newline):
+    filename = str(file_name).replace(".html", "_new.html")
+    print(filename)
+    # file = open(filename, 'a', encoding="utf-8")
+    # file.write(newline)
+    # file.close()
+
+
+file_name = "index.html"
+# read_the_file(file_name)
+
+while True:
+    massage = input("Would you like to remove all tags in index.html? (Type y/yes):\n").lower().strip()
+    if massage == "yes" or massage == "y":
+        remove_tags(file_name)
+    else:
+        print("Thank you for using this program. Goodbye!")
+        break
