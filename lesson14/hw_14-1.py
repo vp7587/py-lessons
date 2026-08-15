de1e49f18647f4eeef7df01891a9112295cddbc0
vp7587 <vp7587@gmail.com>
2026-08-15 10:00:32 +0300

    hw_14-1.0

---
+++ lesson14/hw_14-1.py
@@ -0,0 +1,71 @@
+from asyncio import start_server
+
+
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+    def __str__(self,  *args):
+        return f'{self}'
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+
+    def __str__(self, *args):
+        return f'Name:{self.last_name}'
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, student):
+        self.group.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if last_name == student.last_name:
+                return student
+            return f'Is not in'
+
+    def __str__(self):
+        all_students = ''
+        for student in self.group:
+            all_students += f'{student}\n'
+        return f'Number:{self.number}\n {all_students} '
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+
+
+
+# man = Human('Male', 30, 'Steve', 'Jobs')
+# print (man.age)
+
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+
+print(gr)
+print (str(gr.find_student('Jobs')))
+# assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+# assert gr.find_student('Jobs2') is None, 'Test2'
+# assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод пошуку повинен повертати екземпляр'
+#
+# gr.delete_student('Taylor')
+# print(gr)  # Only one student
+#
+# gr.delete_student('Taylor')  # No error!
